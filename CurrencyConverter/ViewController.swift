c9b4deb0c62c68321c0d424a27c7ba598d390b4a
koseali <kose.ali@std.izu.edu.tr>
2020-11-10 12:39:45 +0300

    Basic api json usage example create a url request and basic view design

--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cadLabel: UILabel!
+    @IBOutlet weak var chfLabel: UILabel!
+    @IBOutlet weak var gbpLabel: UILabel!
+    @IBOutlet weak var jpyLabel: UILabel!
+    @IBOutlet weak var usdLabel: UILabel!
+    @IBOutlet weak var tryLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+     
     }
 
 
+    @IBAction func getRates(_ sender: Any) {
+        
+        // 1-) Reqest & session
+        // 2-) Response & Data
+        // 3-)  Parsin & Json serialization
+        // URLSession.shared.datatask ile de direk alınabilir ama ayrı
+        let url = URL(string: "http://data.fixer.io/api/latest?access_key=80eb126d00e9b7bc8773b90a6a54f6f3")
+        let session = URLSession.shared
+        // closure
+        let task = session.dataTask(with: url!) { (data, respons, error) in
+            if error != nil{
+            let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+            let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+            alert.addAction(okButton)
+            self.present(alert, animated: true, completion: nil)
+            }
+            // 2. step
+            else{
+                if data != nil {
+                    <#code#>
+                }
+            }
+        }
+        
+    }
 }
 

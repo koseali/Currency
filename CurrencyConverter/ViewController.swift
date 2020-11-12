a398096747d4deaa1ec1cd9a5247445a9618b1b1
koseali <kose.ali@std.izu.edu.tr>
2020-11-12 14:56:36 +0300

    add to get data json object with dictionary struct , and print to view.

--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -15,10 +15,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var jpyLabel: UILabel!
     @IBOutlet weak var usdLabel: UILabel!
     @IBOutlet weak var tryLabel: UILabel!
+    @IBOutlet weak var euroLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+      
      
     }
 
@@ -44,16 +45,56 @@ class ViewController: UIViewController {
             else{
                 if data != nil {
                     do {
-                        let jsonResponse = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers)
-                        DispatchQueue.main.async {print(jsonResponse)}
-                    } catch {
-                        print("error")
-                    }
-                  
+                        let jsonResponse = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as! Dictionary<String,Any>
+                        DispatchQueue.main.async {
+                            if let rates = jsonResponse["rates"] as? [String : Any]{
+                                let  tele = rates["TRY"] as! Double
+                                print(tele)
+                                if let tryn = rates["TRY"] as? Double{
+                                    let tryn = tryn / tele
+                                    self.tryLabel.text =  "TRY: \(tryn)"
+                                }
+                                
+                                if var cad = rates["CAD"] as? Double{
+                                    cad = tele / cad
+                                    self.cadLabel.text =  "CAD: \(cad)"
+                                    //"CAD: " + String(cad)
+                                
+                                }
+                                if var chf = rates["CHF"] as? Double{
+                                    chf = tele / chf
+                                    self.chfLabel.text =  "CHF: \(chf)"
+                                }
+                                if var gbp = rates["GBP"] as? Double{
+                                    gbp = tele / gbp
+                                    self.gbpLabel.text =  "GBP: \(gbp)"
+                                }
+                                if var jpy = rates["JPY"] as? Double{
+                                    jpy = tele / jpy
+                                    self.jpyLabel.text =  "JPY: \(jpy)"
+                                }
+                                if var usd = rates["USD"]  as? Double{
+                                     usd = tele / usd
+                                    self.usdLabel.text =  "USD: \(usd)"
+                                    
+                                }
+                                if let euro = rates["TRY"]  as? Double{
+                                    self.euroLabel.text =  "EUR: \(euro)"
+                                    
+                                }
+                                
+                                
+                            }
+                            
+                            
+                        }
+                    } catch {print("error")}
                 }
             }
+            
         }
         task.resume()
+        
         // start to task for all things task.resume() // start to task for all things
     }
     
